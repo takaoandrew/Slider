e78d3099c0ca48d283f0f09b24afdaad884ca61c
Andrew Takao <takaoandrew@gmail.com>
2017-05-01 11:33:16 -0400

    initial commit

--- app/src/main/java/com/andrewtakao/slider/MainActivity.java
+++ app/src/main/java/com/andrewtakao/slider/MainActivity.java
@@ -5,10 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     private SeekBar seekBar;
     private TextView textView;
+    private static final Random RANDOM = new Random();
+    private LineGraphSeries<DataPoint> series;
+    private int Xprev = 0;
+    private int Xcur = 0;
+    private double Yprev = 0;
+    private double Ycur = 0;
+    private int deltaT = 100;
+    private double tau = 1000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +33,22 @@ public class MainActivity extends AppCompatActivity {
         seekBar = (SeekBar) findViewById(R.id.seek_bar);
         textView = (TextView) findViewById(R.id.seek_bar_value);
 
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        // data
+        series = new LineGraphSeries<DataPoint>();
+        graph.addSeries(series);
+        // customize a little bit viewport
+        Viewport viewport = graph.getViewport();
+        viewport.setYAxisBoundsManual(true);
+        viewport.setMinY(0);
+        viewport.setMaxY(100);
+        viewport.setXAxisBoundsManual(true);
+        viewport.setMinX(0);
+        viewport.setMaxX(1000);
+        viewport.setScrollable(true);
+
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-//            int progressValue=0;
+            //            int progressValue=0;
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 //                progressValue = progress;
@@ -36,8 +66,53 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-
-
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // we're going to simulate real time with thread that append data to the graph
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // we add 100 new entries
+                for (int i = 0; i < 1000; i++) {
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            addEntry();
+                        }
+                    });
+
+                    // sleep to slow down the add of entries
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        // manage error ...
+                    }
+                }
+            }
+        }).start();
+    }
+
+    // add random data to graph
+
+    private void addEntry() {
+        // here, we choose to display max 10 points on the viewport and we scroll to end
+//        series.appendData(new DataPoint(lastX++, Integer.parseInt(textView.getText().toString())), false, 1000);
+//        Y += Integer.parseInt(textView.getText().toString())/10;
+
+        Ycur = (Integer.parseInt(textView.getText().toString())-Ycur)*deltaT/tau+Ycur;
+//        Ycur = Integer.parseInt(textView.getText().toString());
+        Xcur += 1;
+
+        series.appendData(new DataPoint(Xcur, Ycur), false, 1000);
+    }
+
+
+
+
 
 }
